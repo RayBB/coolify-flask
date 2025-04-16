491f895bde46c7910987cb7ebb622882b9e5f734
RayBB <RayBB@users.noreply.github.com>
2025-04-16 00:58:03 -0400

    print on healthcheck

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!!</p>"
 
-@app.route('/health') 
-def health(): 
-    return  {"status":"healthy"}, 200
+
+@app.route("/health")
+def health():
+    print("health check called")
+    return {"status": "healthy"}, 200
